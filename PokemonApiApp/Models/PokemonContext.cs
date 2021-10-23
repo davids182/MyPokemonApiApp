94b983c9ce97c696ac801c7fef8bdc53316290fd
David Oliveira <davi.exe@hotmail.com>
2021-10-22 22:27:50 -0300

    Adicionar arquivos de projeto.

---
+++ PokemonApiApp/Models/PokemonContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PokemonApiApp.Models
+{
+    public class PokemonContext : DbContext
+    {
+
+        public PokemonContext(DbContextOptions<PokemonContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pokemon> PokemonItems { get; set; }
+
+    }
+}
